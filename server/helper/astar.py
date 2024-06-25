0c9dfcdcea5abdcd3509cb66c551305ca27ff9b0
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2024-06-26 00:53:57 +0300

    Add algorithm:

---
+++ server/helper/astar.py
@@ -0,0 +1,113 @@
+import logging
+import heapq
+import json
+from graph import Node
+
+
+class AStar:
+    def __init__(self, good_points: list = []):
+        self.open_list = None
+        self.closed_list = None
+        self.start_node = None
+        self.end_node = None
+
+        self.good_points = [0, "EX", "EN"]
+        self.good_points.extend(good_points)
+
+        self.maze = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+
+        file = open("../exampleData/placements.json", "r")
+
+        json_content = json.loads(file.read())
+
+        for content in json_content:
+            self.maze[content["y"]][content["x"]] = content["product_id"]
+
+    def heuristic(self, node1, node2):
+        return abs(node1.x - node2.x) + abs(node1.y - node2.y)
+
+    def get_neighbors(self, node):
+        neighbors = []
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        for direction in directions:
+            new_x, new_y = node.x + direction[0], node.y + direction[1]
+            if 0 <= new_x < len(self.maze) and 0 <= new_y < len(self.maze[0]):
+                if self.maze[new_x][new_y] in self.good_points:
+                    neighbors.append(Node(new_x, new_y))
+        return neighbors
+
+    def reconstruct_path(self, end_node):
+        path = []
+        current = end_node
+        while current is not None:
+            path.append((current.x, current.y))
+            current = current.parent
+        return path[::-1]
+
+    def get_point(self, point: str):
+        for i in range(len(self.maze)):
+            for k in range(len(self.maze[0])):
+                if self.maze[i][k] == point:
+                    return (i, k)
+
+    def search(self, start, end):
+        self.start_node = Node(start[0], start[1])
+        self.end_node = Node(end[0], end[1])
+        self.open_list = []
+        self.closed_list = set()
+
+        heapq.heappush(self.open_list, self.start_node)
+
+        while self.open_list:
+            current_node = heapq.heappop(self.open_list)
+            self.closed_list.add((current_node.x, current_node.y))
+
+            if current_node.x == self.end_node.x and current_node.y == self.end_node.y:
+                path = self.reconstruct_path(current_node)
+
+                return [len(path) - 2, path]
+
+            neighbors = self.get_neighbors(current_node)
+            for neighbor in neighbors:
+                if (neighbor.x, neighbor.y) in self.closed_list:
+                    continue
+
+                tentative_g = current_node.g + 1  # Assuming cost from current to neighbor is always 1
+                if (neighbor.x, neighbor.y) not in [(n.x, n.y) for n in self.open_list]:
+                    neighbor.g = tentative_g
+                    neighbor.h = self.heuristic(neighbor, self.end_node)
+                    neighbor.f = neighbor.g + neighbor.h
+                    neighbor.parent = current_node
+                    heapq.heappush(self.open_list, neighbor)
+                else:
+                    open_neighbor = next((n for n in self.open_list if n.x == neighbor.x and n.y == neighbor.y), None)
+                    if tentative_g < open_neighbor.g:
+                        open_neighbor.g = tentative_g
+                        open_neighbor.f = tentative_g + open_neighbor.h
+                        open_neighbor.parent = current_node
+
+        return None
+
+
