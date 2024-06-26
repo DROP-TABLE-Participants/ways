26fe7236340baddffceb0211e093128bf6f46598
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2024-06-27 00:37:11 +0300

    Fix port error

--- server/redis_client.py
+++ server/redis_client.py
@@ -3,7 +3,7 @@ import redis_om
 
 redis_client = redis_om.get_redis_connection(
     host=os.getenv("REDIS_HOST"),
-    port=os.getenv("REDIS_portT"),
+    port=os.getenv("REDIS_PORT"),
     password=os.getenv("REDIS_PASSWORD"),
     decode_responses=True
 )
