d3d71135d2ff7eaf03a426644a7e09860fcc2f6a
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2024-06-27 21:50:07 +0300

    Post and Delete tiles from admin

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,10 +1,37 @@
+import { useNavigate } from 'react-router-dom';
 import '../styles/page-styles/login.scss';
 
 function Login() {
+    const navigation = useNavigate();
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const username = formData.get('username') as string;
+        const password = formData.get('password') as string;
+
+        fetch('https://ways-api.azurewebsites.net/api/admin/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password }),
+            credentials: 'include'
+        })
+            .then(response => {
+                if (response.ok) {
+                    navigation('/admin');
+                    localStorage.setItem('isLoggedIn', JSON.stringify(true));
+                } else {
+                    alert('Грешно потребителско име или парола');
+                }
+            });
+    }
+
+
     return (
         <div className="login-container">
             <h1>Вход</h1>
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Име</label>
                     <input type="text" id="username" name="username" required />
